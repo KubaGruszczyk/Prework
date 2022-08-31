b69a3d64bd302b255a2dd60e487f7a86850adc98
unknown <j.gruszczyk96@gmail.com>
2022-08-31 19:16:31 +0200

    C# Prework

--- Program.cs
+++ Program.cs
@@ -6,3 +6,30 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("//////////////////////");
 Console.WriteLine("Task 2");
 Console.WriteLine("Hello Jakub!");
+
+//Task 3
+Console.WriteLine("//////////////////////");
+Console.WriteLine("Task 3");
+var name = "Jakub";
+Console.WriteLine("Hello " + name + "!");
+
+//Task 4
+Console.WriteLine("//////////////////////");
+Console.WriteLine("Task 4");
+var user_name = "Jakub";
+Console.WriteLine($"Hello {user_name}!");
+
+//Task 5
+Console.WriteLine("//////////////////////");
+Console.WriteLine("Task 5");
+var username = "Jakub";
+Console.WriteLine($"Hello {username.ToUpper()}!");
+
+//Task 6
+Console.WriteLine("//////////////////////");
+Console.WriteLine("Task 6");
+var names = new[] { "Jakub", "Szymon", "Artur" };
+foreach (var user in names)
+    {
+    Console.WriteLine($"Hello {user}!");
+}
